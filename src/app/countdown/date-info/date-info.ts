f02c3e0da27ea1c380415c6d6542725a4a6ac5ad
M4L5 <malin.gu2017@gmail.com>
2025-09-01 22:09:18 +0200

    Date fields showing target date

---
+++ src/app/countdown/date-info/date-info.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-date-info',
+  imports: [],
+  templateUrl: './date-info.html',
+  styleUrl: './date-info.scss'
+})
+export class DateInfo {
+
+}
