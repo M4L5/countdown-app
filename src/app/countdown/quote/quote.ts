92ffc4cbf81bdb6399bbb3cfef34d6037e9950e6
M4L5 <malin.gu2017@gmail.com>
2025-09-02 21:39:01 +0200

    Added a spinner to the quote component

--- src/app/countdown/quote/quote.ts
+++ src/app/countdown/quote/quote.ts
@@ -1,18 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { QuoteApi } from './quote-api';
-
+import { Spinner } from '../../reusables/spinner/spinner';
 
 @Component({
   selector: 'app-quote',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, Spinner],
   templateUrl: './quote.html',
   styleUrl: './quote.scss'
 })
 export class Quote {
   quote: string = '';
   error: string | null = null;
+  loading = false;
 
   constructor(private quoteService: QuoteApi) {}
 
@@ -21,11 +22,17 @@ export class Quote {
   }
 
   private loadQuote() {
+    this.loading = true;
     this.quoteService.getRandomQuote().subscribe({
       next: (data) => {
         this.quote = data.quote;
+        this.loading = false;
       },
-      error: (err) => console.error('API error:', err)
+      error: (err) => {
+        console.error('API error:', err);
+        this.error = 'Failed to load quote';
+        this.loading = false;
+      }
     });
   }
 }
